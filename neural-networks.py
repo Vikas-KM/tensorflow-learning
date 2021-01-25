3bbaef5de81a67226d87ecc6013458332359e585
Vikas KM <vikki.km@gmail.com>
2021-01-25 12:37:03 +0530

    flower dataset CNN
    Signed-off-by: Vikas KM <vikki.km@gmail.com>

--- neural-networks.py
+++ neural-networks.py
@@ -3,5 +3,9 @@ import os
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.datasets import mnist
+import sys
 
 os.environ['TFF_CPP_MIN_LOG_LEVEL'] = '2'
+
+print(sys.version)
+print(tf.__version__)
