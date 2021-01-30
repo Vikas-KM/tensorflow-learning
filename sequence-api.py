72674cfe229c7fbabac40190195d482a0062f397
Vikas KM <vikki.km@gmail.com>
2021-01-30 20:44:43 +0530

    sequence api, NLP tensorflow code added
    Signed-off-by: Vikas KM <vikki.km@gmail.com>

--- sequence-api.py
+++ sequence-api.py
@@ -1,28 +1,38 @@
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers
 from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 sentences = [
     'I love my Dog',
-    'i love my Cat',
-    'you love my Dog',
-    'Do you love my dog?'
+    'my cat loves me',
+    'Does my dog love me?',
+    'my cat loves my mother more'
 ]
 
-# tokenizer = Tokenizer(num_words=100)
 tokenizer = Tokenizer(num_words=100, oov_token='<OOV>')
 tokenizer.fit_on_texts(sentences)
 word_index = tokenizer.word_index
+print('word index')
 print(word_index)
 
 sequences = tokenizer.texts_to_sequences(sentences)
+print('sequences')
 print(sequences)
+
+padded = pad_sequences(sequences)
+print('Padded')
+print(padded)
 
 test_data = [
     'i really love my dog',
-    'my dog loves my cat'
+    'my cat likes my father'
 ]
 
 test_seq = tokenizer.texts_to_sequences(test_data)
+print('test sequences')
 print(test_seq)
+
+test_pad = pad_sequences(test_seq)
+print('test padded')
+print(test_pad)
